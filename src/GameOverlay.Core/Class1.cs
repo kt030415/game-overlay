f622551d153a6a9c025bc7b88eb5ca0e7d890626
kt030415 <kt030415@outlook.com>
2026-07-07 18:17:24 +0800

    chore: scaffold game overlay solution

---
+++ src/GameOverlay.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GameOverlay.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/GameOverlay.Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameOverlay.Tests
+{
+    internal static class Program
+    {
+        private static int Main()
+        {
+            Console.WriteLine("GameOverlay tests are not implemented yet.");
+            return 1;
+        }
+    }
+}
